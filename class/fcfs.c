3c5e88a0fa041104f10a2c1ac5ebbda551437f7b
Akshay Kumar <akshayrishu4@gmail.com>
2026-02-20 09:24:42 +0530

    fcfs

---
+++ class/fcfs.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+    
+    int bt[n], wt[n], tat[n];
+    
+    printf("Enter Burst Time for each process:\n");
+    for(i = 0; i < n; i++) {
+        printf("Process %d: ", i + 1);
+        scanf("%d", &bt[i]);
+    }
+
+    // First process waiting time is 0
+    wt[0] = 0;
+
+    // Calculate waiting time
+    for(i = 1; i < n; i++) {
+        wt[i] = wt[i - 1] + bt[i - 1];
+    }
+
+    // Calculate turnaround time
+    for(i = 0; i < n; i++) {
+        tat[i] = wt[i] + bt[i];
+    }
+
+    // Display results
+    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for(i = 0; i < n; i++) {
+        printf("P%d\t\t%d\t\t%d\t\t%d\n", 
+                i + 1, bt[i], wt[i], tat[i]);
+    }
+
+    return 0;
+}
